37ba25f61f19d8a23e8a4e1eedfc52e7b3210a1c
Pankaj Shitole <pankajshitole001@gmail.com>
2025-03-20 21:39:57 +1100

    Modifying database schema - adding one table

--- database/onboarding_schema.sql
+++ database/onboarding_schema.sql
@@ -3,6 +3,8 @@ DROP TABLE IF EXISTS melanoma_incidence CASCADE;
 DROP TABLE IF EXISTS melanoma_mortality CASCADE;
 DROP TABLE IF EXISTS vic_suburbs CASCADE;
 DROP TABLE IF EXISTS heat_recorded CASCADE;
+DROP TABLE IF EXISTS agg_uv_per_year CASCADE;
+
 
 -- Create Melanoma Incidence Table
 CREATE TABLE melanoma_incidence (
@@ -28,4 +30,10 @@ CREATE TABLE vic_suburbs (
 CREATE TABLE heat_recorded (
     record_date DATE PRIMARY KEY,
     maximum_temperature DECIMAL(5, 2) NOT NULL
+);
+
+-- Create Average UV Index per year Table
+CREATE TABLE agg_uv_per_year (
+    year INT PRIMARY KEY,
+    mean_uv DECIMAL(5,2) NOT NULL
 );
